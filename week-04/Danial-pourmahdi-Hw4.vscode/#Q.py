f6c256bf911625c3608af8f1f6a829058a05881c
DP-2006 <danialpourmehdi@gmail.com>
2025-10-09 21:13:01 +0330

    Update #Q.py

--- week-04/Danial-pourmahdi-Hw4.vscode/#Q.py
+++ week-04/Danial-pourmahdi-Hw4.vscode/#Q.py
@@ -3,5 +3,7 @@
 def most_repetly(list):
     return max(set(list),key= list.count)
 most_repetly
-print("list")
+my_list = [1,2,3,4,5,66,6,6,7,7,8,8,8,9,10]
+result = most_repetly(my_list)
+print result
 
