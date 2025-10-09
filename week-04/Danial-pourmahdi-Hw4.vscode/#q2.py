02ff793dbfb7ce2a93064fc24a4621e47f1ed955
DP-2006 <danialpourmehdi@gmail.com>
2025-10-09 21:03:46 +0330

    Update #q2.py

--- week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
+++ week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
@@ -1,21 +1,44 @@
 #q2 
-def to_string(funk):
-    def warrer(*args, **kwargs):
-        return funk(*args, **kwargs)
-    return warrer
-def print_lenght(funk):
-    def warrer(*args, **kwargs):
-        return funk(*args, **kwargs)
-    return warrer
+# def to_string(funk):
+#     def warrer(*args, **kwargs):
+#         return funk(*args, **kwargs)
+#     return warrer
+# def print_lenght(funk):
+#     def warrer(*args, **kwargs):
+#         return funk(*args, **kwargs)
+#     return warrer
+
+# @to_string
+# @print_lenght
+
+# def exp_funk(a,b,c="Hi"):
+#     print(f"{a},{b},{c}")
+# exp_funk("Hi", "maktab", "pyhton137")
+
+
+
+def to_string(func):
+    def wrapper(*args, **kwargs):
+        print("to_string")
+        str_args = [str(arg) for arg in args]
+        str_kwargs = {k: str(v) for k, v in kwargs.items()}
+        return func(*str_args, **str_kwargs)
+    return wrapper
+
+def print_length(func):
+    def wrapper(*args, **kwargs):
+        print("print_length")
+        for arg in args:
+            print(f": {len(str(arg))}")
+        return func(*args, **kwargs)
+    return wrapper
 
 @to_string
-@print_lenght
+@print_length
+def exp_func(a, b, c="Hi"):
+    print(f"result: {a}, {b}, {c}")
 
-def exp_funk(a,b,c="Hi"):
-    print(f"{a},{b},{c}")
-exp_funk("Hi", "maktab", "pyhton137")
-
-
+exp_func("Hi", "maktab", "python137")
 
 
         
