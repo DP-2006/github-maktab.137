99f97f42bb8bd3f277df73bfb85e29a4f0b12211
DP-2006 <danialpourmehdi@gmail.com>
2025-10-09 20:37:57 +0330

    Update #q2.py

--- week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
+++ week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
@@ -1,24 +1,19 @@
 #q2 
-def to_string(func):
-    def wrapper(*args, **kwargs):
-        str_args = [str(args) for arg in args]        
-        str_kwargs = {K: str(v) for k, v in kwargs.items()}
-        return func
-    return wrapper
-
-def print_lenght(func):
-    def wrapper(*args,**kwargs):
-        for arg in args:
-            print(f"lenght for arg: {len(arg)}")
-        for k, in kwargs.items:
-            print("lenght for arg v")
-        return func
-    return wrapper
+def to_string(funk):
+    def warrer(*args, **kwargs):
+        return funk(*args, **kwargs)
+    return warrer
+def print_lenght(funk):
+    def warrer(*args, **kwargs):
+        return funk(*args, **kwargs)
+    return warrer
 
 @to_string
 @print_lenght
-def exp_func(a, b, c="Hi"):
-    print(f" a:{a}, b:{b}, c:{c}, d:{d}")
+
+def exp_funk(a,b,c="Hi"):
+    print(f"{a},{b},{c}")
+exp_funk("Hi", "maktab", "pyhton137")
 
 
 
