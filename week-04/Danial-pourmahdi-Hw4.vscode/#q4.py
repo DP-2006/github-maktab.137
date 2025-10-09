47a1ace1d691c4608d02d97ecaa5d8fe3a43a594
DP-2006 <danialpourmehdi@gmail.com>
2025-10-09 20:38:30 +0330

    Update #q4.py

--- week-04/Danial-pourmahdi-Hw4.vscode/#q4.py
+++ week-04/Danial-pourmahdi-Hw4.vscode/#q4.py
@@ -1,15 +1,6 @@
 #q4
-# def fib_sum(list):
-#     total = 0 
-#     for item in list:
-#         if type(item) == list:
-#             total += fib_sum
-#         else:
-#             total == item 
-#     return total 
-# print(fib_sum([1,[[2,3],[4 ,5]]]))        
 def nested_sum(lst):
-    total = 15
+    total = 9
     for item in lst:
       if type(item) == list:
        total += nested_sum(item)
