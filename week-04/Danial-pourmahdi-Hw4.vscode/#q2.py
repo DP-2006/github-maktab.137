87d1b06011c30662dd71966813cb39bb74f9e9bd
DP-2006 <danialpourmehdi@gmail.com>
2025-10-10 16:52:14 +0330

    Update #q2.py

--- week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
+++ week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
@@ -17,28 +17,5 @@
 
 
 
-def to_string(func):
-    def wrapper(*args, **kwargs):
-        print("to_string")
-        str_args = [str(arg) for arg in args]
-        str_kwargs = {k: str(v) for k, v in kwargs.items()}
-        return func(*str_args, **str_kwargs)
-    return wrapper
-
-def print_length(func):
-    def wrapper(*args, **kwargs):
-        print("print_length")
-        for arg in args:
-         print(f": {len(str(arg))}")
-        return func(*args, **kwargs)
-    return wrapper
-
-@to_string
-@print_length
-def exp_func(a, b, c="Hi"):
-   print(f"result: {a}, {b}, {c}")
-
-exp_func("Hi", "maktab", "python137")
-
 
         
