a8b38fcfa44f7fa511794d206c754e4e05216e0e
Danial <danialpourmehdi@gmail.com>
2026-02-14 14:11:43 +0330

    Update #q2.py

--- week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
+++ week-04/Danial-pourmahdi-Hw4.vscode/#q2.py
@@ -1,19 +1,19 @@
-#q2 
-# def to_string(funk):
-#     def warrer(*args, **kwargs):
-#         return funk(*args, **kwargs)
-#     return warrer
-# def print_lenght(funk):
-#     def warrer(*args, **kwargs):
-#         return funk(*args, **kwargs)
-#     return warrer
+q2 
+def to_string(funk):
+    def warrer(*args, **kwargs):
+        return funk(*args, **kwargs)
+    return warrer
+def print_lenght(funk):
+    def warrer(*args, **kwargs):
+        return funk(*args, **kwargs)
+    return warrer
 
-# @to_string
-# @print_lenght
+@to_string
+@print_lenght
 
-# def exp_funk(a,b,c="Hi"):
-#     print(f"{a},{b},{c}")
-# exp_funk("Hi", "maktab", "pyhton137")
+def exp_funk(a,b,c="Hi"):
+    print(f"{a},{b},{c}")
+exp_funk("Hi", "maktab", "pyhton137")
 
 
 
